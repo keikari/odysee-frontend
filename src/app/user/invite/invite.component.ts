40a8211fb9cc1c8b7aea22f8edc400636750a131
Mark Beamer Jr <markbeamerjr@gmail.com>
2019-02-19 21:35:06 -0500

    Added user invite form.

--- src/app/user/invite/invite.component.ts
+++ src/app/user/invite/invite.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {RestService} from '../../rest.service';
 import {MessageService} from 'primeng/api';
+import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-invite',
@@ -16,4 +17,16 @@ export class InviteComponent implements OnInit {
   ngOnInit() {
   }
 
+  inviteUser($event: any) {
+    const params = new HttpParams().
+    set('email', this.email).
+    set('notify', this.notifyValue);
+
+    this.rest.get('user', 'invite', params).subscribe((response: any) => {
+      if (response !== undefined) {
+        this.messageService.clear();
+        this.messageService.add({severity: 'success', summary: 'Success', detail: JSON.stringify(response.data, null, 1)});
+      }
+    });
+  }
 }
