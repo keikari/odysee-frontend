171f490b41474281a8f652524ec60d8556a243d3
Ivan Hranat <e4drcf@gmail.com>
2020-11-05 04:00:37 +0200

    add channel multiply factor to audit

--- src/app/scams/user-detail/user-detail.component.ts
+++ src/app/scams/user-detail/user-detail.component.ts
@@ -1,10 +1,10 @@
-import {Component, EventEmitter, Input, Output, OnInit} from '@angular/core';
-import {User} from './model/user/user';
-import {HttpParams} from '@angular/common/http';
-import {ApiService} from '../../services/api.service';
-import {MessageService} from 'primeng/api';
-import {TagService} from '../../services/tag.service'
-import {YoutubeChannel} from './model/youtube_channel/youtube-channel';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { User } from './model/user/user';
+import { HttpParams } from '@angular/common/http';
+import { ApiService } from '../../services/api.service';
+import { MessageService } from 'primeng/api';
+import { TagService } from '../../services/tag.service'
+import { YoutubeChannel } from './model/youtube_channel/youtube-channel';
 import { Tag } from './model/tag/tag';
 
 @Component({
@@ -16,86 +16,86 @@ export class UserDetailComponent implements OnInit {
   @Output() userReject: EventEmitter<User> = new EventEmitter();
   @Input() DisplayedUser: User;
   userColumns = [
-    {field: 'UserID', header: 'UserID'},
-    {field: 'Duplicates', header: 'Duplicates', width: '13px'},
-    {field: 'Verification', header: 'Verification'},
-    {field: 'RewardStatusChangeTrigger', header: 'Trigger'},
-    {field: 'LastAccessTime', header: 'Last Access'},
-    {field: 'IsCountryMatch', header: 'Country Match'},
-    {field: 'Country', header: 'Country'},
-    {field: 'PrimaryEmail', header: 'Email'}];
+    { field: 'UserID', header: 'UserID' },
+    { field: 'Duplicates', header: 'Duplicates', width: '13px' },
+    { field: 'Verification', header: 'Verification' },
+    { field: 'RewardStatusChangeTrigger', header: 'Trigger' },
+    { field: 'LastAccessTime', header: 'Last Access' },
+    { field: 'IsCountryMatch', header: 'Country Match' },
+    { field: 'Country', header: 'Country' },
+    { field: 'PrimaryEmail', header: 'Email' }];
   emailColumns = [
-    {field: 'EmailAddress', header: 'Email'},
-    {field: 'IsPrimary', header: 'Primary'},
-    {field: 'IsVerified', header: 'Verified'}];
+    { field: 'EmailAddress', header: 'Email' },
+    { field: 'IsPrimary', header: 'Primary' },
+    { field: 'IsVerified', header: 'Verified' }];
   phoneColumns = [
-    {field: 'PhoneNumber', header: 'Number'},
-    {field: 'Country', header: 'Country'}];
+    { field: 'PhoneNumber', header: 'Number' },
+    { field: 'Country', header: 'Country' }];
   youtubeColumns = [
-    {field: 'ChannelName', header: 'Channel Name'},
-    {field: 'LBRYChannelName', header: 'LBRY Channel'},
-    {field: 'Subscribers', header: 'Subscribers'},
-    {field: 'Videos', header: 'Videos'},
-    {field: 'Status', header: 'Status'},
-    {field: 'RewardAmount', header: 'Reward Amount'},
-    {field: 'IsRedeemed', header: 'Redeemed'},
-    {field: 'IsRedeemable', header: 'Redeemable', api_field: 'redeemable'},
-    {field: 'ShouldSync', header: 'ShouldSync', api_field: 'should_sync'},
-    {field: 'Reviewed', header: 'Reviewed', api_field: 'reviewed'}];
+    { field: 'ChannelName', header: 'Channel Name' },
+    { field: 'LBRYChannelName', header: 'LBRY Channel' },
+    { field: 'Subscribers', header: 'Subscribers' },
+    { field: 'Videos', header: 'Videos' },
+    { field: 'Status', header: 'Status' },
+    { field: 'RewardAmount', header: 'Reward Amount' },
+    { field: 'IsRedeemed', header: 'Redeemed' },
+    { field: 'IsRedeemable', header: 'Redeemable', api_field: 'redeemable' },
+    { field: 'ShouldSync', header: 'ShouldSync', api_field: 'should_sync' },
+    { field: 'Reviewed', header: 'Reviewed', api_field: 'reviewed' }];
   creditColumns = [
-    {field: 'CreatedAt', header: 'Created'},
-    {field: 'UpdatedAt', header: 'Updated'}];
+    { field: 'CreatedAt', header: 'Created' },
+    { field: 'UpdatedAt', header: 'Updated' }];
   accessColumns = [
-    {field: 'IP', header: 'IP'},
-    {field: 'ISP', header: 'ISP'},
-    {field: 'Country', header: 'Country'},
-    {field: 'Score', header: 'Score'},
-    {field: 'AccessTime', header: 'Time'}];
+    { field: 'IP', header: 'IP' },
+    { field: 'ISP', header: 'ISP' },
+    { field: 'Country', header: 'Country' },
+    { field: 'Score', header: 'Score' },
+    { field: 'AccessTime', header: 'Time' }];
   redeemedRewardColumns = [
-    {field: 'Type', header: 'Type'},
-    {field: 'Amount', header: 'Amount'},
-    {field: 'CreatedAt', header: 'Created At'},
-    {field: 'Platform', header: 'Platform'},
-    {field: 'TransactionID', header: 'TransactionID'}];
+    { field: 'Type', header: 'Type' },
+    { field: 'Amount', header: 'Amount' },
+    { field: 'CreatedAt', header: 'Created At' },
+    { field: 'Platform', header: 'Platform' },
+    { field: 'TransactionID', header: 'TransactionID' }];
   dupColumns = [
-    {field: 'UserID', header: 'UserID'},
-    {field: 'RewardStatusChangeTrigger', header: 'Trigger'},
-    {field: 'IsRewardsApproved', header: 'Rewards Approved'},
-    {field: 'PrimaryEmail', header: 'Primary Email'},
-    {field: 'FirstIPMatch', header: 'First Matched IP'},
-    {field: 'CreatedAt', header: 'Created at'}];
+    { field: 'UserID', header: 'UserID' },
+    { field: 'RewardStatusChangeTrigger', header: 'Trigger' },
+    { field: 'IsRewardsApproved', header: 'Rewards Approved' },
+    { field: 'PrimaryEmail', header: 'Primary Email' },
+    { field: 'FirstIPMatch', header: 'First Matched IP' },
+    { field: 'CreatedAt', header: 'Created at' }];
   installColumns = [
-    {field: 'Platform', header: 'Platform'},
-    {field: 'DeviceType', header: 'Device'},
-    {field: 'CreatedAt', header: 'Created'},
-    {field: 'UpdatedAt', header: 'Updated'},
-    {field: 'Domain', header: 'Domain'}];
+    { field: 'Platform', header: 'Platform' },
+    { field: 'DeviceType', header: 'Device' },
+    { field: 'CreatedAt', header: 'Created' },
+    { field: 'UpdatedAt', header: 'Updated' },
+    { field: 'Domain', header: 'Domain' }];
   noteColumns = [
-    {field: 'Note', header: 'Note'},
-    {field: 'UpdatedAt', header: 'UpdatedAt'},
-    {field: 'CommenterEmail', header: 'Changed by'}];
+    { field: 'Note', header: 'Note' },
+    { field: 'UpdatedAt', header: 'UpdatedAt' },
+    { field: 'CommenterEmail', header: 'Changed by' }];
   inviterColumns = [
-    {field: 'UserID', header: 'UserID'},
-    {field: 'PrimaryEmail', header: 'Email'},
-    {field: 'RewardStatusChangeTrigger', header: 'Trigger'},
-    {field: 'RewardEnabled', header: 'Rewards Enabled'},
-    {field: 'IsEmailVerified', header: 'Verified'},
-    {field: 'TotalRedeemedRewards', header: 'Redeemed Rewards'},
-    {field: 'InvitedUsers', header: 'Invited Users'},
-    {field: 'InviteRewards', header: 'Invite Rewards'},
+    { field: 'UserID', header: 'UserID' },
+    { field: 'PrimaryEmail', header: 'Email' },
+    { field: 'RewardStatusChangeTrigger', header: 'Trigger' },
+    { field: 'RewardEnabled', header: 'Rewards Enabled' },
+    { field: 'IsEmailVerified', header: 'Verified' },
+    { field: 'TotalRedeemedRewards', header: 'Redeemed Rewards' },
+    { field: 'InvitedUsers', header: 'Invited Users' },
+    { field: 'InviteRewards', header: 'Invite Rewards' },
   ];
   ownedChannelsColumns = [
-    {field: 'URI', header: 'URI'},
-    {field: 'SignedStreams', header: 'Signed Streams'},
-    {field: 'ViewRateFactor', header: 'View Rate Factor'}
+    { field: 'URI', header: 'URI' },
+    { field: 'SignedStreams', header: 'Signed Streams' },
+    { field: 'ViewRateFactor', header: 'View Rate Factor' }
   ];
   invitedUsersColumns = [
-    {field: 'UserID', header: 'UserID'},
-    {field: 'PrimaryEmail', header: 'Email'},
-    {field: 'RewardStatusChangeTrigger', header: 'Trigger'},
-    {field: 'RewardEnabled', header: 'Rewards Enabled'},
-    {field: 'IsEmailVerified', header: 'Verified'},
-    {field: 'TotalRedeemedRewards', header: 'Redeemed Rewards'},
+    { field: 'UserID', header: 'UserID' },
+    { field: 'PrimaryEmail', header: 'Email' },
+    { field: 'RewardStatusChangeTrigger', header: 'Trigger' },
+    { field: 'RewardEnabled', header: 'Rewards Enabled' },
+    { field: 'IsEmailVerified', header: 'Verified' },
+    { field: 'TotalRedeemedRewards', header: 'Redeemed Rewards' },
   ];
   selectedTag: any;
   tagOptions: any[] = [];
@@ -105,38 +105,38 @@ export class UserDetailComponent implements OnInit {
   }
 
   ngOnInit() {
-      this.tagService.getTags().subscribe(response => {
-        if (response !== undefined) {
-          response.data.forEach((tag) => {
-            this.tagOptions = this.tagOptions.concat({name: tag.name, value: tag.name});
-            this.DisplayedUser.Tags.forEach((userTag)=> {
-              if (userTag.Id == tag.id) {
-                userTag.Name = tag.name;
-              }
-            })
-          });
-          this.selectedTag = this.tagOptions[0];
-        }
-      });
+    this.tagService.getTags().subscribe(response => {
+      if (response !== undefined) {
+        response.data.forEach((tag) => {
+          this.tagOptions = this.tagOptions.concat({ name: tag.name, value: tag.name });
+          this.DisplayedUser.Tags.forEach((userTag) => {
+            if (userTag.Id == tag.id) {
+              userTag.Name = tag.name;
+            }
+          })
+        });
+        this.selectedTag = this.tagOptions[0];
+      }
+    });
   }
 
   reject(user: User) {
     const params = new HttpParams().
-    set('id', user.UserID.toString()).
-    set('comment', 'Commander Rejected!').
-    set('is_reward_approved', 'no');
+      set('id', user.UserID.toString()).
+      set('comment', 'Commander Rejected!').
+      set('is_reward_approved', 'no');
 
     this.rest.get('user', 'approve', params).subscribe((response) => {
       if (response && response.error) {
         this.messageService.clear();
-        this.messageService.add({severity: 'error', summary: 'Error', detail: response.error});
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: response.error });
       } else if (response && response.data) {
         this.messageService.clear();
-        this.messageService.add({severity: 'success', summary: 'Rejected', detail: 'User rejected for rewards!'});
+        this.messageService.add({ severity: 'success', summary: 'Rejected', detail: 'User rejected for rewards!' });
         this.userReject.emit(user);
       } else {
         this.messageService.clear();
-        this.messageService.add({severity: 'error', summary: 'No Response Data?', detail: ''});
+        this.messageService.add({ severity: 'error', summary: 'No Response Data?', detail: '' });
       }
     });
   }
@@ -144,37 +144,50 @@ export class UserDetailComponent implements OnInit {
   // param came from column.api_field
   changeFieldStatus(channel: YoutubeChannel, param: string, event) {
     const params = new HttpParams().
-    set('channel_id', channel.ChannelID.toString()).
-    set(param, event.checked.toString());
+      set('channel_id', channel.ChannelID.toString()).
+      set(param, event.checked.toString());
     this.rest.get('yt', 'disapprove', params).subscribe((response) => {
       if (response && response.error) {
         this.messageService.clear();
-        this.messageService.add({severity: 'error', summary: 'Error', detail: response.error});
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: response.error });
       } else if (response && response.data) {
         this.messageService.clear();
-        this.messageService.add({severity: 'success', summary: 'Changed', detail: 'Channel edited!'});
+        this.messageService.add({ severity: 'success', summary: 'Changed', detail: 'Channel edited!' });
       } else {
         this.messageService.clear();
-        this.messageService.add({severity: 'error', summary: 'No Response Data?', detail: ''});
+        this.messageService.add({ severity: 'error', summary: 'No Response Data?', detail: '' });
       }
     });
   }
 
-  changeTagStatus(id :bigint, tag: Tag) {
-    let changedStatus = !tag.IsRemoved;
+  changeTagStatus(id: bigint, tag: Tag) {
+    const changedStatus = !tag.IsRemoved;
     for (let i = 0; i < this.DisplayedUser.Tags.length; i++) {
-      if (this.DisplayedUser.Tags[i].Id == tag.Id)
-        this.DisplayedUser.Tags[i].IsRemoved = changedStatus
+      if (this.DisplayedUser.Tags[i].Id === tag.Id) {
+        this.DisplayedUser.Tags[i].IsRemoved = changedStatus;
+      }
     }
     this.tagUser(tag.Name, id, changedStatus);
   }
 
   tagUser(tag: string, id: bigint, status: boolean) {
-    this.tagService.tagUser(tag,id.toString(),status).subscribe((response: any) => {
-        if (response !== undefined) {
-          this.messageService.clear();
-          this.messageService.add({severity: 'success', summary: 'Success', detail: JSON.stringify(response.data, null, 1)});
-        }
-      });
+    this.tagService.tagUser(tag, id.toString(), status).subscribe((response: any) => {
+      if (response !== undefined) {
+        this.messageService.clear();
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: JSON.stringify(response.data, null, 1) });
+      }
+    });
+  }
+  setFactor(url: string, factor: number) {
+    const claimId = url.slice(-40);
+    const params = new HttpParams().
+      set('channel_claim_id', claimId).
+      set('factor', factor.toString());
+    this.rest.get('creator', 'set_factor', params).subscribe((response: any) => {
+      if (response !== undefined) {
+        this.messageService.clear();
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: JSON.stringify(response.data, null, 1) });
+      }
+    });
   }
 }
