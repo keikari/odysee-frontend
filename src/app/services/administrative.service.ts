ae3ada9404f1bb8389f1d03f275a45dc86bbd509
Ivan Hranat <e4drcf@gmail.com>
2021-01-13 09:45:39 +0100

    add administrative service

---
+++ src/app/services/administrative.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {ApiService} from './api.service';
+import {Observable} from 'rxjs';
+import {HttpParams} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdministrativeService {
+
+  constructor(public rest: ApiService) {
+  }
+  getPendingYTChannels(): Observable<any> {
+    return this.rest.get('administrative', 'youtube_pending', new HttpParams());
+  }
+}
