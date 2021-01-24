7c13efa9275420d3574c0fe9786d5f1e54cec45a
Mark Beamer Jr <markbeamerjr@gmail.com>
2021-01-24 14:55:39 -0500

    Login before building docker image

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # .env should contain docker username and password with auth for internal-api docker repo
 touch -a .env && set -o allexport; source ./.env; set +o allexport
-docker build --tag lbry/commander:$TRAVIS_BRANCH ./
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+docker build --tag lbry/commander:$TRAVIS_BRANCH ./
 docker push lbry/commander:$TRAVIS_BRANCH
