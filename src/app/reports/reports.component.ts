f3fac014a268cc78dacc48856abb363a54416f5e
Ivan Hranat <e4drcf@gmail.com>
2021-05-25 09:34:57 +0300

    fix tagging + add limit on reports

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,11 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {Report} from './model/report'
-import {User} from "../scams/user-detail/model/user/user";
-import {ReportService} from "../services/report.service";
-import {HttpParams} from "@angular/common/http";
-import {MessageService} from "primeng";
-import {TagService} from "../services/tag.service";
-import {ApiService} from "../services/api.service";
+import {Report} from './model/report';
+import {ReportService} from '../services/report.service';
+import {HttpParams} from '@angular/common/http';
+import {MessageService} from 'primeng';
+import {TagService} from '../services/tag.service';
+import {ApiService} from '../services/api.service';
 
 @Component({
   selector: 'app-reports',
@@ -30,11 +29,15 @@ export class ReportsComponent implements OnInit {
   selectedTag: any;
   userID: string;
 
-  constructor(public rest: ReportService, private messageService: MessageService, private tagService: TagService, public apiRest: ApiService) {
+
+  constructor(public rest: ReportService,
+              private messageService: MessageService,
+              private tagService: TagService,
+              public apiRest: ApiService) {
   }
 
   ngOnInit(): void {
-    this.changeColumns(null)
+    this.changeColumns(null);
     this.tagService.getTags().subscribe(response => {
       if (response !== undefined) {
         response.data.forEach((tag) => {
@@ -49,7 +52,7 @@ export class ReportsComponent implements OnInit {
           this.messageService.clear();
           this.messageService.add({severity: 'error', summary: 'Error', detail: response.error});
         } else if (response && response.success) {
-          this.userID = response.data.id
+          this.userID = response.data.id;
         } else {
           this.messageService.clear();
           this.messageService.add({severity: 'error', summary: 'No Response Data?', detail: ''});
@@ -63,7 +66,11 @@ export class ReportsComponent implements OnInit {
   loadReports() {
     this.reports = [];
     let params = new HttpParams().set('status', this.status);
-
+    if (this.itemsToReturnFilter) {
+      params = params.set('limit', this.itemsToReturnFilter.toString());
+    } else {
+      params = params.set('limit', '99999');
+    }
 
     this.rest.getAction('list', params).subscribe((reportResponse) => {
       if (reportResponse && reportResponse.error) {
@@ -81,25 +88,25 @@ export class ReportsComponent implements OnInit {
         this.messageService.add({severity: 'error', summary: 'No Response Data?', detail: ''});
       }
       this.loading = false;
-    })
+    });
   }
 
   changeColumns(reviewed) {
-    let baseColumns = [
+    const baseColumns = [
       {field: 'ReportID', header: 'ID'},
       {field: 'Type', header: 'Type'},
       {field: 'Category', header: 'Category'},
       {field: 'ClaimID', header: 'ClaimID'},
       {field: 'Status', header: 'Status'},
       {field: 'Email', header: 'Email'},
-    ]
+    ];
     if (reviewed != null) {
-      let additionalColumns = [
+      const additionalColumns = [
         {field: 'IsActionTaken', header: 'IsActionTaken'},
         {field: 'ReviewedById', header: 'ReviewedById'},
         {field: 'ReviewerComment', header: 'ReviewerComment'},
-      ]
-      this.reportColumns = baseColumns.concat(additionalColumns)
+      ];
+      this.reportColumns = baseColumns.concat(additionalColumns);
     } else {
       this.reportColumns = baseColumns;
     }
@@ -109,20 +116,20 @@ export class ReportsComponent implements OnInit {
     this.isReviewing = true;
     this.review = report;
     this.Details = JSON.parse(this.review.Details);
-    this.User = report.User
+    this.User = report.User;
   }
 
   confirmReview(status) {
-    let begining = this.userID + " Report#" + this.review.ReportID
+    const begining = this.userID + ' Report#' + this.review.ReportID;
     if (!this.review.ReviewerComment) {
-      this.review.ReviewerComment = ""
+      this.review.ReviewerComment = '';
     }
-    this.review.ReviewerComment = begining + " " + this.review.ReviewerComment
+    this.review.ReviewerComment = begining + ' ' + this.review.ReviewerComment;
     let params = new HttpParams()
       .set('report_id', this.review.ReportID.toString())
       .set('comment', this.review.ReviewerComment).set('status', status);
     if (this.useCustomTag && this.selectedTag.name) {
-      params = params.set('tag', this.selectedTag.name)
+      params = params.set('tag', this.selectedTag.value);
     }
     this.rest.getAction('review', params).subscribe((response) => {
       if (response && response.error) {
@@ -136,7 +143,7 @@ export class ReportsComponent implements OnInit {
         this.messageService.clear();
         this.messageService.add({severity: 'error', summary: 'No Response Data?', detail: ''});
       }
-    })
+    });
     this.isReviewing = false;
   }
 
