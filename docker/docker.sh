f604b6f47b03816ea9dfc60625e32b439a4f5714
Mark Beamer Jr <markbeamerjr@gmail.com>
2022-02-03 14:09:50 -0500

    move commander to new docker hub repository.

--- docker/docker.sh
+++ docker/docker.sh
@@ -2,5 +2,5 @@
 # .env should contain docker username and password with auth for internal-api docker repo
 touch -a .env && set -o allexport; source ./.env; set +o allexport
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-docker build --tag lbry/commander:$TRAVIS_BRANCH ./
-docker push lbry/commander:$TRAVIS_BRANCH
+docker build --tag odyseeteam/commander:$TRAVIS_BRANCH ./
+docker push odyseeteam/commander:$TRAVIS_BRANCH
